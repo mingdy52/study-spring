28a6c244763275917d115eda3e5999c0262e3810
mingdy <mincount552@gmail.com>
2024-03-24 23:26:23 +0900

    섹션 3 서블릿, jsp, mvc패턴

---
+++ src/main/java/hello/sevlet/web/sevletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,36 @@
+package hello.sevlet.web.sevletmvc;
+
+import hello.sevlet.domain.member.Member;
+import hello.sevlet.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(req, resp);
+
+    }
+}
