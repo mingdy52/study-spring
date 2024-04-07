f265f2c6a02aef6dc29c62ecd76fe723d623b3be
mingdy <mincount552@gmail.com>
2024-04-07 18:56:52 +0900

    프론트 컨트롤러, view 분리

---
+++ src/main/java/hello/sevlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,34 @@
+package hello.sevlet.web.frontcontroller.v1.controller;
+
+import hello.sevlet.domain.member.Member;
+import hello.sevlet.domain.member.MemberRepository;
+import hello.sevlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher(viewPath);
+        requestDispatcher.forward(req, resp);
+    }
+}
+
