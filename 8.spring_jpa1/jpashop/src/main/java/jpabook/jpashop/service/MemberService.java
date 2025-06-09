b4884351d6a8b1fa5b5830a7890c9bfb56b1958b
mingdy <mincount552@gmail.com>
2025-06-09 22:58:44 +0900

    회원 등록, 수정, 조회 API

--- 8.spring_jpa1/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ 8.spring_jpa1/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -43,4 +43,17 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    /**
+        회원 수정
+          변경 감지를 사용해서 데이터를 수정
+
+        PUT은 전체 업데이트를 할 때 사용한다.
+        부분 업데이트를 하려면 PATCH를 사용하거나 POST를 사용하는 것이 REST 스타일에 맞다.
+     */
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
